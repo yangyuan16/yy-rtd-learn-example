48cb3a88f42222905e111195f3c36b22ff689188
yangyuan16_glb <50294636+yangyuan16@users.noreply.github.com>
2022-06-24 14:48:32 +0800

    update 20220624

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -30,6 +30,7 @@ release = '0.0.4'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
+import sphinx_markdown_tables
 extensions = ['sphinx.ext.todo', 'sphinx.ext.viewcode', 'sphinx.ext.autodoc',
               'sphinx.ext.napoleon',
               'recommonmark',
